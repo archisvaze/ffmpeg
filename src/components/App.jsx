37cdcbe00cb6f977b493a38c45a730dbf580c403
archis <vazearchis@gmail.com>
2022-12-13 14:19:42 +0530

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='container'>
+      <header>
+        <h1>FFmpeg Tutorial</h1>
+      </header>
+
+      <main>
+        <h2>Main</h2>
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default App
