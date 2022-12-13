05bef5e80ec9734b4d388e48fc6990d869c0c93a
archis <vazearchis@gmail.com>
2022-12-13 14:26:12 +0530

    layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import Main from './Main'
 
 const App = () => {
   return (
     <div id='container'>
-      <header><h1>FFmpeg Tutorial</h1></header>
-      <main>
-
-
-
-        <h2>Main</h2>
-
-
-
-      </main>
-      <footer>Footer</footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main>
+            <h2>Main</h2>
+
+
+
+
+        </main>
+    )
+}
+
+export default Main
