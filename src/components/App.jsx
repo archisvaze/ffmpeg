08d03eadebb666f120088437a3f7984df3c757f9
archis <vazearchis@gmail.com>
2022-12-13 14:20:34 +0530

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const App = () => {
   return (
     <div id='container'>
-      <header>
-        <h1>FFmpeg Tutorial</h1>
-      </header>
-
+      <header><h1>FFmpeg Tutorial</h1></header>
       <main>
-        <h2>Main</h2>
-      </main>
 
+
+
+        <h2>Main</h2>
+
+
+
+      </main>
       <footer>Footer</footer>
     </div>
   )
